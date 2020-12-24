b3000f71175bbabead645422a99a7a9c1a371c11
unknown <officialbidisha1@gmail.com>
2020-12-24 17:45:55 +0530

    Third Commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log( new tutorial.SomeMathObject());
 
 
 //Events Module and Events Emitter Class
-/*const EventEmitter = require ('events');
+const EventEmitter = require ('events');
 const eventEmitter = new EventEmitter();
 eventEmitter.on('tutorial',(num1,num2)=>{ // will only be executed when a tutorial event occurs
     console.log('tutorial event has occured');
@@ -31,7 +31,7 @@ pedro.emit('name');
 christina.emit('name');//Synchronous execution of events occur in node.js*/
 
 //READLINE module
-/*const readLine = require('readline');
+const readLine = require('readline');
 const rl= readLine.createInterface({input:process.stdin, output:process.stdout});
 let num1 = Math.floor((Math.random()*10)+1);
 let num2= Math.floor((Math.random()*10)+1);
@@ -61,10 +61,10 @@ rl.question(`What is ${num1} + ${num2}? \n`,
 );
 rl.on('close',()=>{
     console.log('Correct!!');
-});*/
+});
 //File System Module
-//const fs= require('fs');
-/*fs.writeFile('example.txt','This is an example',(err)=>{
+const fs= require('fs');
+fs.writeFile('example.txt','This is an example',(err)=>{
     if(err){
         console.log('error');
     }
@@ -79,33 +79,33 @@ rl.on('close',()=>{
             }
         })
     }
-})*/
+})
 
-/*fs.rename('example.txt','example2.txt', (err)=>{
+fs.rename('example.txt','example2.txt', (err)=>{
    if(err){
        console.log(err);
    }
    else{
        console.log('successfully renamed file');
    }
-})*/
-/*fs.appendFile('example2.txt','Some data being appended',(err)=>{
+})
+fs.appendFile('example2.txt','Some data being appended',(err)=>{
     if(err){
         console.log(err);
     }
     else{
         console.log('successfully appended data to file');
     }
-})*/
-/*fs.unlink('example2.txt',(err)=>{
+})
+fs.unlink('example2.txt',(err)=>{
     if(err){
         console.log(err);
     }
     else{
         console.log('successfully deleted file');
     }
-})*/
-/* fs.mkdir('tutorial',(err)=>{// folder creation and multiple file creation within folder
+})
+ fs.mkdir('tutorial',(err)=>{// folder creation and multiple file creation within folder
     if(err){
         console.log(err);
     }
@@ -128,9 +128,9 @@ rl.on('close',()=>{
         })
         console.log('folder successfully created');
     }
-})*/
+})
 
-/*fs.unlink('./tutorial/example.txt',(err)=>{
+fs.unlink('./tutorial/example.txt',(err)=>{
     if(err){
         console.log(err);
     }
@@ -144,16 +144,16 @@ rl.on('close',()=>{
             }
         })
     }
-})*/
-/*fs.rmdir('tutorial',(err)=>{ // folder deletion
+})
+fs.rmdir('tutorial',(err)=>{ // folder deletion
     if(err){
         console.log(err);
     }
     else{
         console.log('folder successfully deleted');
     }
-})*/
-/*fs.readdir('tutorial',(err,files)=>{//delete multiple files within the folder
+})
+fs.readdir('tutorial',(err,files)=>{//delete multiple files within the folder
     if(err){
         console.log(err);
     }
@@ -170,30 +170,30 @@ rl.on('close',()=>{
             })
         }
     }
-})*/
-//const fs= require('fs');
-//const readStream = fs.createReadStream('example.txt', 'utf-8');
-//const writeStream = fs.createWriteStream('example2.txt')
-/*readStream.on('data', (chunk)=>{
+})
+const fs= require('fs');
+const readStream = fs.createReadStream('example.txt', 'utf-8');
+const writeStream = fs.createWriteStream('example2.txt')
+readStream.on('data', (chunk)=>{
     console.log(chunk);// on getting chunk we can manipulate it 
     // sending data to a new file while reading it. Hence chunk is useful
     writeStream.write(chunk);
 
-})*/ //alternative is pipe chaining
+}) //alternative is pipe chaining
 //Need to use Streams - For large files, using readFile doesn't help because a buffer of such big size 
 // is not present. 
 
 /*Pipes and Pipes Chaining */
-//readStream.pipe(writeStream); 
+readStream.pipe(writeStream); 
 //Pipe Chainling
 
-/*const zlib = require('zlib');
+const zlib = require('zlib');
 const gzip = zlib.createGzip();// creating zip file
 const writeStream = fs.createWriteStream('example2.txt.gz');
-readStream.pipe(gzip).pipe(writeStream);*/
+readStream.pipe(gzip).pipe(writeStream);
 
 /*HTTP Module */
-/*const http = require('http');
+const http = require('http');
 const server = http.createServer((req,res)=>{
     if(req.url==='/'){
         res.write('Hello World from Node.js');
@@ -204,18 +204,19 @@ const server = http.createServer((req,res)=>{
         res.end();
     }
 });
-server.listen(3000);*/
+server.listen(3000);
 /*Serving Static files using HTTP and File System Module */
-/*const http = require('http');
+const http = require('http');
 const fs= require('fs');
 const { type } = require('os');
 const server = http.createServer((req,res)=>{
     const readStream = fs.createReadStream('./static/example.json');
     res.writeHead(200, {'Content-type':'application/json'})
     readStream.pipe(res);
-}).listen(3000);*/
+}).listen(3000);
 
-
+/*
+Server static file serving using express.js
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
@@ -232,3 +233,14 @@ app.get('/example/:name/:age',(req,res)=>{
     
 })
 app.listen(3001);
+*/
+/*Server on express.js*/
+
+const express = require ('express');
+const path = require ('path');
+const app = express();
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static','index.html'))
+});
+app.listen(3000);
